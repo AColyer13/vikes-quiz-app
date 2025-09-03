dd44c8ff8c7bfcd3814f8c7d4c7cd8c49e2e84ad
Adam <adamcolyer@gmail.com>
2025-09-03 03:01:03 -0500

    Change build dir to dist and add GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vikes-quiz-app/'
+  base: '/vikes-quiz-app/',
+  build: {
+    outDir: 'dist'
+  }
 })
